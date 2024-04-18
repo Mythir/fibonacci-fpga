10148671cc39ba0c9868c0942d492d8ea275d3a4
Mythir <larsvanleeuwen94@gmail.com>
2024-04-18 09:49:28 +0200

    Clean up

--- hw.srcs/sources_1/new/opcodes.vhd
+++ hw.srcs/sources_1/new/opcodes.vhd
@@ -10,4 +10,7 @@ package opcodes is
     constant OP_ADD : std_logic_vector(OPCODE_WIDTH-1 downto 0) := "0001";
     constant OP_ADDI : std_logic_vector(OPCODE_WIDTH-1 downto 0) := "0010";
     constant OP_STORE : std_logic_vector(OPCODE_WIDTH-1 downto 0) := "0011";
+
+    constant R1: std_logic_vector(OPCODE_WIDTH-1 downto 0) := "0001";
+    constant R2: std_logic_vector(OPCODE_WIDTH-1 downto 0) := "0010";
 end package opcodes;
--- hw.srcs/sources_1/new/program_rom.vhd
+++ hw.srcs/sources_1/new/program_rom.vhd
@@ -27,18 +27,58 @@ end program_rom;
 
 architecture arch of program_rom is
     --Todo: rom_type
+    type rom_type is array(0 to (2**ADDRESS_WIDTH-1)) of std_logic_vector(DATA_WIDTH-1 downto 0);
 
     --Todo: memory
+    constant mem: rom_type := (
+        OP_STORE & R1 & "00000000",
+
+        OP_ADDI & R1 & "0001" & R2, --[0,1]
+        OP_STORE & R2 & "00000000",
+        OP_ADD & R1 & R2 & R1, --[1,1]
+        OP_STORE & R1 & "00000000",
+
+        OP_ADD & R1 & R2 & R2, --[1,2]
+        OP_STORE & R2 & "00000000",
+        OP_ADD & R1 & R2 & R1, --[3,2]
+        OP_STORE & R1 & "00000000",
+
+        OP_ADD & R1 & R2 & R2,
+        OP_STORE & R2 & "00000000",
+        OP_ADD & R1 & R2 & R1,
+        OP_STORE & R1 & "00000000",
+
+        OP_ADD & R1 & R2 & R2,
+        OP_STORE & R2 & "00000000",
+        OP_ADD & R1 & R2 & R1
+
+    )
 
     -- Todo: input register
+    signal input_register: std_logic_vector(ADDRESS_WIDTH-1 downto 0);
 begin
     --Todo: handshake
+    in_ready <= out_ready;
 
     --Todo: uitlezen mem
+    data_out <= mem(conv_integer(unsigned(input_register)));
 
     process(clk) is
     begin
     --Todo: reset, handshake out, handshake in
+        if rising_edge(clk) then
+            if reset = '1' then
+                out_valid <='0';
+            else
+                if out_ready = '1' and out_valid = '1' then
+                    out_valid <='0';
+                end if;
+
+                if in_ready = '1' and in_valid = '1' then
+                    input_register <= address;
+                    out_valid <= '1';
+                end if;
+        end if;
     end process;
  
 end arch;
